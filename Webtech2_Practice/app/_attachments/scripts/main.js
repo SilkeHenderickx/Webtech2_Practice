8c4540d48f21a0d68a3d3c11726849fd40dbd0da
SilkeHenderickx <silke.henderickx@student.ap.be>
2017-11-07 14:20:13 +0100

    checkGrades implementation

--- Webtech2_Practice/app/_attachments/scripts/main.js
+++ Webtech2_Practice/app/_attachments/scripts/main.js
@@ -14,5 +14,50 @@ function addGrade(){
 	var json = JSON.stringify(doc);
 	console.log(json);
 	
+	$.ajax({
+		type: 'PUT',
+		url: '../../' + name,
+		data: json,
+		contentType: 'application/json',
+		async: true,
+		success: function(data){
+			console.log(data);
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			console.log(errorThrown);
+		}
+	});
+}
+
+function checkGrades(){
+	var grades;
 	
+	var ALL_DOCS = '../../_all_docs?include_docs=true';
+	
+	$.ajax({
+		type: 'GET',
+		url: ALL_DOCS,
+		contentType: 'application/json',
+		async: true,
+		success: function(data){
+			console.log(data);
+			var arr = JSON.parse(data).rows;
+			console.log(arr);
+			var htmlString = '<ul>';
+			for(var i=0;i<arr.length;i++){
+				
+				if(arr[i].doc.type === 'student'){
+					var doc = arr[i].doc;
+					htmlString += '<li>Name: ' + doc.name+ ', Course: ' + doc.course+ ', Grade: ' + doc.grade +'</li>';
+				}
+				
+			}
+			htmlString += '</ul>';
+			console.log(htmlString);
+			$("#grades").html(htmlString);
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){
+			console.log(errorThrown);
+		}
+	});
 }
